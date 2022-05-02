c93de00558a39cd75bd595c5f1c0e0583121ee0e
Erildojs <patriciopatrick92@gmail.com>
2022-05-02 18:46:07 +0100

    refactor add funcinality of show and hidde something when i click on icon

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -12,7 +12,7 @@ export function Widget() {
         <div className="absolute bottom-5 right-5">
         { isWidgetOpen && <p>hello world</p>}
 
-        <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
             <ChatTeardropDots   className="w-6 h-6"/>
 
             <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
